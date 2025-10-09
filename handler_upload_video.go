1a09b9139b008df1582eed66aa5d9713dc54a9de
evanwiseman <evanwiseman2000@gmail.com>
2025-10-08 19:22:13 -0700

    added video upload to s3

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,142 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	// Set max upload memory 1GB
+	const maxMemory = 1 << 30
+	r.Body = http.MaxBytesReader(w, r.Body, maxMemory)
+
+	err := r.ParseMultipartForm(32 << 20) // 32 MB memory buffer, rest spills to disk
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to parse multipart form", err)
+		return
+	}
+
+	// Get the videoID
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	// Get the JWT Token
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	// Validate the JWT Token
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	// Get the metadata metadata
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+
+	// Validate user owns video
+	if metadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User not authorized", err)
+		return
+	}
+
+	// Parse uploaded video
+	videoFile, videoHeader, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer videoFile.Close()
+
+	// Get the media type and validate it's a mp4
+	mediaType, _, err := mime.ParseMediaType(videoHeader.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid media type", err)
+		return
+	}
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Unsupported media type", errors.New("unsupported type"))
+		return
+	}
+
+	// Create a temporary file
+	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to make file", err)
+		return
+	}
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	// Stream copy
+	if _, err = io.Copy(tempFile, videoFile); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to copy file", err)
+		return
+	}
+
+	// Move to the start of the file
+	if _, err = tempFile.Seek(0, io.SeekStart); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to seek to start", err)
+		return
+	}
+
+	// Create s3 key
+	bytes := make([]byte, 32)
+	_, err = rand.Read(bytes)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to fill bytes", err)
+		return
+	}
+	key := base64.RawURLEncoding.EncodeToString(bytes) + ".mp4"
+	cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         aws.String(key),
+		Body:        tempFile,
+		ContentType: aws.String(mediaType),
+	})
+
+	videoURL := fmt.Sprintf("https://%v.s3.%v.amazonaws.com/%v", cfg.s3Bucket, cfg.s3Region, key)
+	video := database.Video{
+		ID:           metadata.ID,
+		CreatedAt:    metadata.CreatedAt,
+		UpdatedAt:    time.Now(),
+		ThumbnailURL: metadata.ThumbnailURL,
+		VideoURL:     &videoURL,
+		CreateVideoParams: database.CreateVideoParams{
+			Title:       metadata.Title,
+			Description: metadata.Description,
+			UserID:      metadata.UserID,
+		},
+	}
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update video", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, video)
+}
